e56487b1c17b33d7491eae3750d9c4954e4f1d32
kimtaehyun98 <kimtaehyun98@naver.com>
2022-01-14 23:25:42 +0900

    Mentos Server 기본 Template

---
+++ src/test/java/MentosServer/mentos/MentosApplicationTests.java
@@ -0,0 +1,13 @@
+package MentosServer.mentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
