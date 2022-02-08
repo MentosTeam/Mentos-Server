f79f786ef137a9906a91f6d50d197922f44e18bf
eunjee <rira9817@gmail.com>
2022-02-09 00:22:11 +0900

    재가입 기능 추가

--- src/main/java/MentosServer/mentos/repository/SchoolCertificationRepository.java
+++ src/main/java/MentosServer/mentos/repository/SchoolCertificationRepository.java
@@ -30,7 +30,7 @@ public class SchoolCertificationRepository {
 	
 	// 이메일 확인
 	public int checkEmail(String email) {
-		String checkEmailQuery = "select exists(select memberemail from member where memberemail = ?)"; // User Table에 해당 email 값을 갖는 유저 정보가 존재하는가?
+		String checkEmailQuery = "select exists(select memberemail from member where memberemail = ?,memberStatus='active')"; // User Table에 해당 email 값을 갖는 유저 정보가 존재하는가?
 		String checkEmailParams = email; // 해당(확인할) 이메일 값
 		return this.jdbcTemplate.queryForObject(checkEmailQuery, int.class, checkEmailParams); // checkEmailQuery, checkEmailParams를 통해 가져온 값(intgud)을 반환한다. -> 쿼리문의 결과(존재하지 않음(False,0),존재함(True, 1))를 int형(0,1)으로 반환됩니다.
 	}
