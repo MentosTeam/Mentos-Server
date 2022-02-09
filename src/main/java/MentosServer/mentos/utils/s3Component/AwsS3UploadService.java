8b7d2cf291efc896c64e0d0294d197ff2fa2d074
eunjee <rira9817@gmail.com>
2022-02-10 08:04:44 +0900

    s3 config 수정

--- src/main/java/MentosServer/mentos/utils/s3Component/AwsS3UploadService.java
+++ src/main/java/MentosServer/mentos/utils/s3Component/AwsS3UploadService.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
@@ -12,23 +13,24 @@ import java.io.InputStream;
 @Component
 public class AwsS3UploadService implements UploadService{
     private final AmazonS3Client amazonS3;
-    private final S3Component component;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket; //S3 버킷 이름
 
     @Override
     public void uploadFile(InputStream inputStream, ObjectMetadata objectMetadata, String fileName) {
-        amazonS3.putObject(new PutObjectRequest(component.getBucket(),fileName,inputStream,objectMetadata)
+        amazonS3.putObject(new PutObjectRequest(bucket,fileName,inputStream,objectMetadata)
                 .withCannedAcl(CannedAccessControlList.PublicRead));
 
     }
 
     @Override
     public String getFileUrl(String fileName) {
-        return amazonS3.getUrl(component.getBucket(),fileName).toString();
+        return amazonS3.getUrl(bucket,fileName).toString();
     }
 
     @Override
     public void deleteFile(String fileName) {
-        amazonS3.deleteObject(new DeleteObjectRequest(component.getBucket(), fileName));
+        amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
 
     }
 }
