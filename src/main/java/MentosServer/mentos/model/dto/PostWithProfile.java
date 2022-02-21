c07ee1cecb6b8ac6bb0dbc6421136951bce86bfc
kimtaehyun98 <kimtaehyun98@naver.com>
2022-02-21 21:36:01 +0900

    멘토 글 찾기 API 반환값 수정

--- src/main/java/MentosServer/mentos/model/dto/PostWithProfile.java
+++ src/main/java/MentosServer/mentos/model/dto/PostWithProfile.java
@@ -19,6 +19,8 @@ public class PostWithProfile implements Comparable<PostWithProfile>{
 	
 	private String memberMajor;
 	
+	private String mentoImage;
+	
 	private String postTitle;
 	
 	private String postContents;
