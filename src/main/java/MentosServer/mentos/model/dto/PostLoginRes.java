47bae737a8d59e15a9a1156195a8419678fea5c5
eunjee <rira9817@gmail.com>
2022-03-02 01:46:34 +0900

    로그인 dto 수정 (#81)

--- src/main/java/MentosServer/mentos/model/dto/PostLoginRes.java
+++ src/main/java/MentosServer/mentos/model/dto/PostLoginRes.java
@@ -17,5 +17,6 @@ public class PostLoginRes {
     private String mentorImage;
     private String menteeImage;
     private int genderFlag; //성별 공개 여부 1이면 on 0이면 off
-    private int notificationFlag; //푸시알림 설정 1이면 on 0이면 off
+    private int mentorNotificationFlag; //푸시알림 설정 1이면 on 0이면 off
+    private int menteeNotificationFlag;
 }
