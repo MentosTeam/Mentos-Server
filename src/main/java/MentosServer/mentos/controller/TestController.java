0891ec7492128552ef3bb875d3a827cb133a1d9e
kimtaehyun98 <kimtaehyun98@naver.com>
2022-03-22 20:59:23 +0900

    자동 배포 설정

--- src/main/java/MentosServer/mentos/controller/TestController.java
+++ src/main/java/MentosServer/mentos/controller/TestController.java
@@ -1,7 +1,14 @@
 package MentosServer.mentos.controller;
 
+import MentosServer.mentos.config.BaseResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
+	
+	@GetMapping("/deployTest")
+	public BaseResponse<String> deployTest(){
+		return new BaseResponse<>("deployTest");
+	}
 }
